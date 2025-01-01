15190beeec0241d85b1ab92a01f0f1afaee7a293
1gilmar <1gilmar@gmail.com>
2025-01-01 11:35:07 -0300

    Ajustado banco de dados, e entidade

--- Transacoes/Program.cs
+++ Transacoes/Program.cs
@@ -38,8 +38,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
 app.UseSwagger();
 app.UseSwaggerUI();
